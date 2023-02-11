0bd96ce1e3c8076d9e329a2ac349b61a9e41e9ad
chenDev <chen@okos.ca>
2023-02-11 12:50:28 -0700

    add DropdownButtonFormField for choosing sugars

--- lib/screens/home/brewForm.dart
+++ lib/screens/home/brewForm.dart
@@ -13,8 +13,10 @@ class BrewForm extends StatefulWidget {
 class _BrewFormState extends State<BrewForm> {
   final _formKey = GlobalKey<FormState>();
 
-  String _curSugars='';
-  String _curName='';
+  final List<int> sugarList = [0, 1, 2, 3, 4];
+
+  String _curSugars = '';
+  String _curName = '';
   int? _curStrength;
 
   bool loading = false;
@@ -22,41 +24,62 @@ class _BrewFormState extends State<BrewForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: loading?const Loading(): Container(
-        child: Column(
-          children: [
-            Text("Update your brew settings"),
-            const SizedBox(
-              height: 20.0,
+      child: loading
+          ? const Loading()
+          : Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 40.0),
+                child: Column(
+                  children: [
+                    Text("Update your brew settings"),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: "Name"),
+                      validator: (value) =>
+                          value!.isEmpty ? "Enter a name " : null,
+                      onChanged: ((value) {
+                        setState(() {
+                          _curName = value;
+                        });
+                      }),
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    //drop down
+                    DropdownButtonFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: "Sugars"),
+                        items: sugarList.map((e) {
+                          return DropdownMenuItem(
+                              value: e, child: Text('$e sugar(s)'));
+                        }).toList(),
+                        onChanged: ((value) {
+                          setState(() {
+                            _curSugars = '$value';
+                          });
+                        })),
+                    //slider
+                    ElevatedButton.icon(
+                      icon: const Icon(Icons.update),
+                      label: const Text("Update"),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.brown[500])),
+                      onPressed: () {
+                        print(_curName);
+                        print(_curSugars);
+                        print(_curStrength);
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
-            TextFormField(
-              decoration: textInputDecoration.copyWith(hintText: "Name"),
-              validator: (value) => value!.isEmpty ? "Enter a name " : null,
-              onChanged: ((value) {
-                setState(() {
-                  _curName = value;
-                });
-              }),
-            ),            
-            const SizedBox(
-              height: 20.0,
-            ),
-            //drop down
-            //slider
-            ElevatedButton.icon(
-              icon: const Icon(Icons.update),
-              label: const Text("Update"),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.brown[500])),
-              onPressed: ()  {
-                print(_curName);
-                print(_curSugars);
-                print(_curStrength);
-              },
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
